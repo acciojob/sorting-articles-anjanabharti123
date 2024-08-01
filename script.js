c1151ee2ed6c45a3f2c98415079e93e3e3acd126
Anjana Bharti <143540468+anjanabharti123@users.noreply.github.com>
2024-08-01 16:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const bands = [
+    'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
+    'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
+    'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
+    'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+];
+
+// Words to ignore for sorting
+const ignoreWords = ["a", "an", "the"];
+
+// Function to remove ignore words from a string
+const removeIgnoreWords = (str) => {
+    return str
+        .toLowerCase()
+        .split(' ')
+        .filter(word => !ignoreWords.includes(word))
+        .join(' ');
+};
+
+// Function to sort bands
+const sortBands = (bands) => {
+    return bands.slice().sort((a, b) => {
+        const strA = removeIgnoreWords(a);
+        const strB = removeIgnoreWords(b);
+        return strA.localeCompare(strB);
+    });
+};
+
+// Get sorted bands
+const sortedBands = sortBands(bands);
+
+// Log the sorted bands to the console
+console.log(sortedBands);
